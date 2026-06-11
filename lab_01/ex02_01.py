2e86a9323e3fa74433b07e22b6fdbf0a84f8bdec
maiquocviet <vietmaiquoc2k5@gmail.com>
2026-06-11 09:40:13 +0700

    lab_04

--- lab_01/ex02_01.py
+++
@@ -1,5 +0,0 @@
-#nhap ten va tuoi tu nguoi dung
-ten = input("nhap ten cua ban: ")
-tuoi = input("nhap tuoi cua ban :")
-#in thong diep chao mung voi thong tin vua nhap
-print("chao mung ", ten ,"! ban ", tuoi,"tuoi.")
--- lab_01/ex02_02.py
+++
@@ -1,6 +0,0 @@
-#nhap ban kinh tu nguoi dung 
-ban_kinh = float(input("nhap ban kinh cua hinh tron:"))
-#tinh dien tich cua hinh tron
-dien_tich = 3.14 *(ban_kinh**2)
-#in dien tich cua hinh tron ra man hinh 
-print("dien tich cua hinh tron la ",dien_tich)
